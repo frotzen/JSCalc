ce39b04d439a08f0a0d2240ad4bc2814810a0612
Elwin Beall <frotzen@gmail.com>
2023-01-03 20:01:19 -0600

    Added partial calculator functionality to app.html

--- js/site.js
+++ js/site.js
@@ -1,4 +1,34 @@
-function displayMessage() {
+/* JS Calc/
+/*********/
+
+// Setup DOM objects
+const form = document.getElementById("calcForm");
+const output = document.getElementById("output");
+const operandBtns = document.querySelectorAll("button[data-type=operand]");
+
+// Disable default submit action
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+// Take input and display it
+let isOperator = false;
+operandBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (output.value == "0") {
+      output.value = e.target.value;
+    } else if (output.value.includes(".")) {
+      output.value = output.value + "" + e.target.value.replace(".", "");
+    } else if (isOperator) {
+      isOperator = false;
+      output.value = e.target.value;
+    } else {
+      output.value = output.value + "" + e.target.value;
+    }
+  });
+});
+/*  REMOVED SweetAlerts
+  function displayMessage() {
   let myMessage = document.getElementById("message").value;
   //alert(myMessage);
 
@@ -8,3 +38,4 @@ function displayMessage() {
     text: myMessage,
   });
 }
+*/
